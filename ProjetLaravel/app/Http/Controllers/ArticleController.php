375338056087808ad6b08e7b3c4e7c086fd639b4
quenette1 <leo.quenette@etu.unilim.fr>
2023-10-20 06:29:44 -0700

    Ajout de la supprression d'article

--- ProjetLaravel/app/Http/Controllers/ArticleController.php
+++ ProjetLaravel/app/Http/Controllers/ArticleController.php
@@ -52,7 +52,7 @@ class ArticleController extends Controller
      */
     public function show(Article $article)
     {
-        return view("articles.show", compact("article"));
+        return view("articles.index", compact("article"));
     }
 
     /**
@@ -84,7 +84,7 @@ class ArticleController extends Controller
         ]);
     
         // 4. On affiche le Post modifié : route("posts.show")
-        return redirect(route("articles.show", $post));
+        return redirect(route("articles.index", $post));
     }
 
     /**
--- ProjetLaravel/resources/views/welcome.blade.php
+++ ProjetLaravel/resources/views/welcome.blade.php
@@ -13,11 +13,34 @@
     </head>
     <body class="antialiased">
         <!-- Banderole en Haut -->
-        <div class="flex w-screen h-28 bg-beige items-center px-4 space-x-4">
+        <div class="flex w-screen h-28 bg-beige items-center px-4 space-x-4 justify-center">
             <h1 class="flex-1 text-white text-2xl font-LaBelleAurore">Ma région danse !</h1>
             @auth
             <a href="{{ url('/dashboard') }}" class="font-semibold hover:text-gray-900 text-white dark:hover:text-grey ">Tableau de bord</a>
             <a href="{{ url('/profile') }}" class="font-semibold hover:text-gray-900 text-white">{{ auth()->user()->name }}</a>
+            <div class="flex flex-line justify-center items-center space-x-8 pt-8">
+                @foreach($articles as $article)
+                    <div class="flex flex-wrap flex-col bg-gray-300 justify-left p-4 border-4 border-gray-300 hover:border-blue-600 w-fit rounded-xl">
+                        <div class="bg-grey text-2xl font-bold">
+                            {{$article->name}}
+                        </div>
+                        <div>
+                            {{$article->description}}
+                        </div>
+                        <div class="text-blue-700">
+                            {{$article->emplacement}}
+                        </div>
+                        <form action="{{route('articles.destroy',$article)}}" method="POST" >
+                            @csrf
+                            @method("delete")
+                            <input class="hidden"></input>
+                            <x-secondary-button class="flex justify-left items-left " type="submit">
+                                <div class="">Del</div>
+                            </x-secondary-button>
+                        </form>
+                    </div>
+                @endforeach
+            </div>
         </div>
         <div>
             @else
@@ -27,24 +50,7 @@
                 </div>
             @endauth
         </div>
-        <div class="flex flex-line justify-center items-center space-x-8 pt-8">
-            @foreach($articles as $article)
-                <div class="flex flex -wrap flex-col bg-gray-300 justify-left p-4 border-4 border-gray-300 hover:border-blue-600 w-28 rounded-xl">
-                    <div class="bg-grey text-2xl font-bold">
-                        {{$article->name}}
-                    </div>
-                    <div>
-                        {{$article->description}}
-                    </div>
-                    <div class="text-blue-700">
-                        {{$article->emplacement}}
-                    </div>
-                    <x-secondary-button href="{{route('articles.destroy',['article',$article])}}" class="flex justify-left items-left ">
-                        <div class="">Del</div>
-                    </x-secondary-button>
-                </div>
-            @endforeach
-        </div>
+        
         
     </body>
 </html>
